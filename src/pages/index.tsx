caf4169c8dd19c127a789c294179068284b8ca08
better_320 <3020naaeun@gmail.com>
2024-05-24 19:37:13 +0900

    [Chore]초기 세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
-
 export default function Home() {
+  // 회원가입 및 로그인 만들기
   return (
-    <main>
+    <>
+      {/* 회원가입 폼 태그 만들기 */}
       <div>Hello, world</div>
-    </main>
+    </>
   );
 }
